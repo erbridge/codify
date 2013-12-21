fee1e260e4d872f519ed9312768fb1400ca4289c
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-12-21 02:03:16 +0000

    Export a png and choose a drawing method.
    Refactor drawing into a subclass.
    Expose a painter option.
    Remove the overlap hack.
    Expose a png export option.
    Make the default number of lines 1.

--- codify.py
+++ codify.py
@@ -9,78 +9,85 @@ from bitarray import bitarray
 
 class Painter:
 
-    @staticmethod
-    def draw_black(context, width, height):
-        context.set_source_rgb(0, 0, 0)
-        Painter._draw_rectangle(context, width, height)
-
-    @staticmethod
-    def draw_white(context, width, height):
-        context.set_source_rgb(1, 1, 1)
-        Painter._draw_rectangle(context, width, height)
-
-    @staticmethod
-    def _draw_rectangle(context, width, height):
-        # Add 1 to ensure there's no visible gap between "1"s
-        width += 1
-        height += 1
+    def _draw_rectangle(self, context, width, height):
         context.rel_line_to(width, 0)
         point = context.get_current_point()
         context.rel_line_to(0, height)
         context.rel_line_to(-width, 0)
         context.close_path()
         context.fill()
-        context.move_to(point[0] - 1, point[1])
+        context.move_to(point[0], point[1])
+
+
+class NaivePainter(Painter):
+
+    def draw_rectangle(self, bit, context, width, height):
+        if bit:
+            rgb = (0, 0, 0)
+        else:
+            rgb = (1, 1, 1)
+
+        context.set_source_rgb(*rgb)
+        self._draw_rectangle(context, width, height)
 
 
 class SVGGenerator:
 
     @staticmethod
-    def run(input_str, num_lines, width, height, output_filename):
-        context = cairo.Context(
-            cairo.SVGSurface(output_filename, width, height))
+    def run(input_str, num_lines, width, height, output_filename, painter,
+            export_filename):
+        surface = cairo.SVGSurface(output_filename, width, height)
+        context = cairo.Context(surface)
 
-        line_height = height / num_lines
+        line_height = int(height / num_lines)
 
         bit_array = bitarray()
         bit_array.fromstring(input_str)
         bit_str = bit_array.to01()
 
-        line_char_width = math.ceil(len(bit_str) * 1.0 / num_lines)
+        line_char_width = math.ceil(len(bit_str) / num_lines)
         lines = [bit_str[i:i + line_char_width]
                  for i in range(0, len(bit_str), line_char_width)]
 
-        box_width = width / line_char_width
+        box_width = int(width / line_char_width)
 
-        current_y = -1
+        current_y = 0
         for line in lines:
-            # Start at -1 to cancel out the extra point added in `Painter`
-            # to ensure there's no visible gap between "1"s
-            context.move_to(-1, current_y)
+            context.move_to(0, current_y)
             for bit in line:
-                if int(bit):
-                    Painter.draw_black(context, box_width, line_height)
-                else:
-                    Painter.draw_white(context, box_width, line_height)
+                painter.draw_rectangle(
+                    int(bit), context, box_width, line_height)
             current_y += line_height
+
+        if export_filename:
+            surface.write_to_png(export_filename)
+
 
 if __name__ == "__main__":
     import getopt
     import sys
 
-    usage = "codify.py -i <input_str> [-o <output_filename> -n <num_lines> -W <width> -H <height>]"
+    usage = "codify.py -i <input_str> [-o <output_filename> \n" + \
+        "-n <num_lines> -W <width> -H <height> -p <painter> \n" + \
+        "-e <export_filename]"
 
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], "hi:o:n:W:H:", ["input_str=", "output_filename=", "num_lines=", "width=", "height="])
+        opts, _ = getopt.getopt(
+            sys.argv[1:],
+            "hi:o:n:W:H:p:e:",
+            ["input_str=", "output_filename=", "num_lines=", "width=",
+             "height=", "painter=", "export="])
     except getopt.GetoptError:
         print(usage)
         sys.exit(1)
 
     input_str = ""
     output_filename = "output.svg"
-    num_lines = 3
+    num_lines = 1
     width = 600
     height = 200
+    painter = NaivePainter()
+    export_filename = ""
 
     for opt, arg in opts:
         if opt == "-h":
@@ -93,12 +100,21 @@ if __name__ == "__main__":
         elif opt in ("-n", "--num_lines"):
             num_lines = int(arg)
         elif opt in ("-W", "--width"):
-            width = float(arg)
+            width = int(arg)
         elif opt in ("-H", "--height"):
-            height = float(arg)
+            height = int(arg)
+        elif opt in ("-p", "--painter"):
+            try:
+                painter = globals()[arg]()
+            except KeyError:
+                print(usage)
+                sys.exit(1)
+        elif opt in ("-e", "--export"):
+            export_filename = arg
 
     if not input_str:
         print(usage)
         sys.exit(1)
 
-    SVGGenerator.run(input_str, num_lines, width, height, output_filename)
+    SVGGenerator.run(input_str, num_lines, width, height, output_filename,
+                     painter, export_filename)
