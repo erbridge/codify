a4e8019baf0c466525a5b2a3aeee3dd37fe6dbe5
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-12-21 02:43:40 +0000

    Update the tests.

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./codify.py -i "Test string" && [ -f "output.svg" ]
+./codify.py -i "Test string" -o test.svg -p NaivePainter && [ -f "test.svg" ] && rm test.svg
+./codify.py -i "Test string" -o test.svg -p ManchesterPainter && [ -f "test.svg" ] && rm test.svg
+./codify.py -i "Test string" -o test.svg -p DifferentialManchesterPainter && [ -f "test.svg" ] && rm test.svg
